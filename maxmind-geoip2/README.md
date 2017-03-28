9d963fcd41724ef63e3c13c0b9ab666b093487f5
Jonas Odencrants <jonas.odencrants@xlent.se>
2017-03-29 01:40:05 +0200

    Create maxmind geoip image (ATEAM-101)

---
+++ maxmind-geoip2/README.md
@@ -0,0 +1,8 @@
+# GeoIP API
+
+Sets up GeoIP API Service.
+
+## Environment vars 
+
+      MAXMIND_USER_ID: < user id >
+      MAXMIND_LICENSE_KEY: < license key >
---
+++ maxmind-geoip2/start-geoip-service.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+echo "UserId ${MAXMIND_USER_ID}">/usr/local/etc/GeoIP.conf;
+echo "LicenseKey ${MAXMIND_LICENSE_KEY}">> /usr/local/etc/GeoIP.conf;
+echo "ProductIds GeoIP2-City GeoIP2-Country">> /usr/local/etc/GeoIP.conf;
+
+nohup /etc/init.d/nginx start  > /dev/null 2>&1 &
+# It will take some seconds before the service is accessible
+/usr/local/bin/geoipupdate && /go/bin/geoip-service -db=/usr/local/share/GeoIP/GeoIP2-City.mmdb -listen=':5000'
